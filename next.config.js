9fccd095958e259994988ea5bcba8cde6fd59e18
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2024-06-27 13:27:46 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com', 'images.unsplash.com','cdn.sanity.io'],
+  },
+};
+
+module.exports = nextConfig;
